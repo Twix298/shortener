c80a53176f6b30e5c63a380507476c050e1282d6
Eremin Aleksandr <eremin.it@gmail.com>
2025-01-15 21:51:54 +0300

    finish part 1

--- internal/app/url_generator/shorter.go
+++ internal/app/url_generator/shorter.go
@@ -4,15 +4,12 @@ import (
 	"encoding/base64"
 )
 
-func EncodeURL(url string) string {
-	str := base64.StdEncoding.EncodeToString([]byte(url))
-	return str
+type Generator struct {
+	Lenght int
 }
 
-func DecodeURL(encodeUrl string) (string, error) {
-	data, err := base64.StdEncoding.DecodeString(encodeUrl)
-	if err != nil {
-		return "", err
-	}
-	return string(data), nil
+func (g *Generator) EncodeURL(url string) string {
+	str := base64.StdEncoding.EncodeToString([]byte(url))
+	str = str[0:g.Lenght]
+	return str
 }
